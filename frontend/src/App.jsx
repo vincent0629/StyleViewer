0ff4eb90231976661323b1f137d2a916113d46c9
Vincent Kao <vincent0629@gmail.com>
2024-01-06 18:28:49 +0800

    Implement frontend.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,106 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const API = 'http://localhost:3000';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [nodeNames, setNodeNames] = useState();
+  const [nodes, setNodes] = useState();
+
+  const fetchNode = (name) => {
+    return fetch(`${API}/get/${name}`)
+      .then(node => node.json());
+  };
+
+  const parentName = (node) => {
+    if (node.parent)
+      return node.parent;
+    else {
+      const p = node.name.lastIndexOf('.');
+      if (p > 0)
+        return node.name.substring(0, p);
+    }
+    return null;
+  };
+
+  const fetchNodeFamily = (name) => {
+    if (!name)
+      return Promise.resolve(null);
+    return new Promise((resolve) => {
+      fetchNode(name)
+        .then(node => {
+          if (!node)
+            resolve(null);
+          else {
+            fetchNodeFamily(parentName(node))
+              .then((parent) => {
+                if (parent)
+                  resolve([node, ...parent]);
+                else
+                  resolve([node]);
+              });
+          }
+        })
+    });
+  }
+
+  const onInputChange = (e) => {
+    if (e.target.value.length >= 3)
+      fetch(`${API}/find/${e.target.value}`)
+        .then(nodes => nodes.json())
+        .then(json => {
+          setNodeNames(json);
+        });
+  };
+
+  const onNodeNameClick = (e) => {
+    fetchNodeFamily(e.target.getAttribute('data-tag'))
+      .then(nodes => {
+        setNodes(nodes);
+      });
+  };
+
+  const renderNodeName = (name) => {
+    return (
+      <div key={`name-${name}`} data-tag={name} onClick={onNodeNameClick}>
+        {name}
+      </div>
+    );
+  };
+
+  const renderAttribute = (attr) => {
+    return (
+      <div key={attr[0]}>
+        <span>{attr[0]}</span>
+        <span>{attr[1]}</span>
+      </div>
+    );
+  };
+
+  const renderNode = (node) => {
+    return (
+      <div key={`node-${node.name}`}>
+        <div>{node.name}</div>
+        <div>
+          {node.attributes && Object.entries(node.attributes).map(attr => renderAttribute(attr))}
+        </div>
+      </div>
+    );
+  };
 
   return (
-    <>
+    <div className="flex">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div className="p-2">
+          <input className="w-full" type="text" placeholder="Type style name" onChange={onInputChange} />
+        </div>
+        {nodeNames && nodeNames.map(name => renderNodeName(name))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        {nodes && nodes.map(node => renderNode(node))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
